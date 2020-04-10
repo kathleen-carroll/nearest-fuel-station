df0629e6d92a1859d0288de1e53277a380161dc2
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-10 09:28:10 -0600

    Write initial test and create controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,5 @@
+class SearchContoller < ApplicationController
+  def index
+    require "pry"; binding.pry
+  end
+end
--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -7,4 +7,21 @@ describe "user can visit the welcome page" do
     expect(page).to have_content("Search For The Nearest Electric Charging Station")
     expect(page).to have_button("Find Nearest Station")
   end
+
+  describe "click on search and see nearest fuel stations" do
+    visit '/'
+
+    click_on "Find Nearest Fuel Station"
+
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_content("Nearest Fuel Station")
+    expect(page).to have_content("Name: ")
+    expect(page).to have_content("Address: ")
+    expect(page).to have_content("Fuel Type: ")
+    expect(page).to have_content("Distance: 0.1 miles")
+    expect(page).to have_content("1 minute away")
+    expect(page).to have_content("Directions: ")
+    expect(page).to have_content("Turn left onto Lawrence St Destination will be on the left")
+  end
 end
