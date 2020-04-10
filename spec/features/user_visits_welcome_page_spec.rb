662832b193ecbb935ceb18f4b64f7e2192bdcec3
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-10 09:51:56 -0600

    Update test

--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -8,10 +8,10 @@ describe "user can visit the welcome page" do
     expect(page).to have_button("Find Nearest Station")
   end
 
-  describe "click on search and see nearest fuel stations" do
+  it "click on search and see nearest fuel stations" do
     visit '/'
 
-    click_on "Find Nearest Fuel Station"
+    click_on "Find Nearest Station"
 
     expect(current_path).to eq('/search')
 
@@ -19,6 +19,7 @@ describe "user can visit the welcome page" do
     expect(page).to have_content("Name: ")
     expect(page).to have_content("Address: ")
     expect(page).to have_content("Fuel Type: ")
+    expect(page).to have_content("Access Times: ")
     expect(page).to have_content("Distance: 0.1 miles")
     expect(page).to have_content("1 minute away")
     expect(page).to have_content("Directions: ")
