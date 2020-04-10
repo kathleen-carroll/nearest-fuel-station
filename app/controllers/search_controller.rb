e5e7a53fe65132e3a5bf07ea15289d465c359eb5
Kathleen Carroll <khakicarroll@comcast.net>
2020-04-10 09:52:19 -0600

    Create connection to NREL api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,11 @@
-class SearchContoller < ApplicationController
+class SearchController < ApplicationController
   def index
-    require "pry"; binding.pry
+    search_term = params[:location]
+
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['NREL_API_KEY']}&location=#{search_term}&fuel_type=ELEC&limit=1")
+    response = conn.get
+    json = JSON.parse(response.body)
+
+    @station = Station.new(json)
   end
 end
